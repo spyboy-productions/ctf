5b1d0696fc67f6e24fdb114983647e9df3e3932d
Spyboy <25564020+thisisshubhamkumar@users.noreply.github.com>
2024-09-30 14:06:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,25 @@
 const container = document.getElementById("container");
-const imageOne = document.querySelector(".image-1");
-const imageTwo = document.querySelector(".image-2");
-const btnYes = document.querySelector(".btn-yes");
 const btnNo = document.querySelector(".btn-no");
+const btnYes = document.querySelector(".btn-yes");
 
 function getRandomNumber(min, max) {
-  // Calculate the random number between min and max (inclusive)
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  return randomNumber;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 btnNo.addEventListener("mouseover", (event) => {
-  const containerHeight = container.getBoundingClientRect().height;
-  const containerWidth = container.getBoundingClientRect().width;
-  const btnHeight = btnNo.getBoundingClientRect().height;
-  const btnWidth = btnNo.getBoundingClientRect().width;
-  const btnTop = btnNo.getBoundingClientRect().top;
-  const btnLeft = btnNo.getBoundingClientRect().left;
+  const containerHeight = container.clientHeight;
+  const containerWidth = container.clientWidth;
+  const btnHeight = btnNo.clientHeight;
+  const btnWidth = btnNo.clientWidth;
 
-  let newTop = btnTop;
-  let newLeft = btnLeft;
-  while (Math.abs(newTop - btnTop) < containerHeight / 3) {
-    newTop = getRandomNumber(0, containerHeight - btnHeight);
-  }
+  let newTop = getRandomNumber(0, containerHeight - btnHeight);
+  let newLeft = getRandomNumber(0, containerWidth - btnWidth);
 
-  while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
-    newLeft = getRandomNumber(0, containerWidth - btnWidth);
-  }
-
-  btnNo.style.top = Math.floor(newTop) + "px";
-  btnNo.style.left = Math.floor(newLeft) + "px";
+  btnNo.style.top = `${newTop}px`;
+  btnNo.style.left = `${newLeft}px`;
 });
 
 btnYes.addEventListener("click", (e) => {
   btnNo.classList.add("hide");
-  imageOne.classList.add("hide");
-  imageTwo.classList.remove("hide");
+  // Assuming you have logic to show images
 });
